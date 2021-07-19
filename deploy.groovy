4c08c73632bf4fa4cb6ffdbedd90ca163261a5b3
ศุภชัย ใจบาน <fotopiece@gmail.com>
2021-07-19 21:07:12 +0700

    first commit

--- deploy.groovy
+++ deploy.groovy
@@ -1,5 +1,9 @@
-print "Hello World!\n"
-print "ddd"
-git branch: 'master',
-    credentialsId: 'a2432c58-e773-4594-a45f-e6ddd1d60e41',
-    url: 'https://github.com/fotopieces/myAngular.git'
+
+pipeline {
+
+    /* Specify where entire pipeline will execute */
+    // agent any
+    agent {
+      label 'my-k8s'
+    }
+}
