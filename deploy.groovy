be8d54aa2cdce1c9977975e6484b2da8e88ad939
ศุภชัย ใจบาน <fotopiece@gmail.com>
2021-07-20 06:33:00 +0700

    first commit

--- deploy.groovy
+++ deploy.groovy
@@ -8,9 +8,10 @@ pipeline {
     }
     stages {
          stage('Checkout Code Angular') {
-                    git branch: 'master',
-                    credentialsId: 'a2432c58-e773-4594-a45f-e6ddd1d60e41',
-                    url: 'https://github.com/fotopieces/myweb.git'
+            steps {
+                git branch: 'master',
+                credentialsId: 'a2432c58-e773-4594-a45f-e6ddd1d60e41',
+                url: 'https://github.com/fotopieces/myweb.git'
             }
         }
          stage('Build Angular') {
